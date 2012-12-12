61442b493e1b0d5dd10462532b5a7d39183d2a66
AhnSeongHyun <sh84.ahn@gmail.com>
2012-12-12 11:39:34 +0900

    [기능추가] splitter 추가

---
+++ src/org/espressootr/lib/utils/Splitter.java
@@ -0,0 +1,13 @@
+package org.espressootr.lib.utils;
+
+public class Splitter
+{
+
+    public static final String COLON = ":";
+    public static final String SEMI_COLON = ";";
+    public static final String PERIOD_REGX = "[.]";
+    public static final String PERIOD = ".";
+    public static final String COMMA = ",";
+    public static final String SPACE = " ";
+    public static final String NEWLINE = "\n";
+}
