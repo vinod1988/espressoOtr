b50e073bd3200649b6ec5a8e228851523537f6a0
AhnSeongHyun <sh84.ahn@gmail.com>
2013-03-05 17:29:55 +0900

    [modify] QuickSort comparator private static final

--- src/main/java/org/espressootr/lib/sort/QuickSort.java
+++ src/main/java/org/espressootr/lib/sort/QuickSort.java
@@ -6,87 +6,89 @@ import java.util.List;
 
 public class QuickSort
 {
-    public static void sort(List<String> sortingTarget)
-    {
-        Comparator<String> comparator = new Comparator<String>() {
-            
-            public int compare(String arg0, String arg1)
-            {
-                
-                if (arg0 == null || arg1 == null) { return 0; }
-                
-                return arg0.compareTo(arg1);
-            }
-        };
+    
+    private static final Comparator<String> stringComp = new Comparator<String>() {
         
-        Collections.sort(sortingTarget, comparator);
+        public int compare(String arg0, String arg1)
+        {
+            
+            if (arg0 == null || arg1 == null) { return 0; }
+            
+            return arg0.compareTo(arg1);
+        }
+    };
+    
+    private static final Comparator<Integer> intAscComp = new Comparator<Integer>() {
+        
+        public int compare(Integer arg0, Integer arg1)
+        {
+            
+            int result = 0;
+            
+            if (arg0 < arg1)
+            {
+                result = -1;
+            }
+            else if (arg0 > arg1)
+            {
+                result = 1;
+            }
+            else
+            {
+                result = 0;
+            }
+            
+            return result;
+        }
+        
+    };
+    
+    private static final Comparator<Integer> intDescComp = new Comparator<Integer>() {
+        
+        public int compare(Integer arg0, Integer arg1)
+        {
+            
+            int result = 0;
+            
+            if (arg0 < arg1)
+            {
+                result = 1;
+            }
+            else if (arg0 > arg1)
+            {
+                result = -1;
+            }
+            else
+            {
+                result = 0;
+            }
+            
+            return result;
+        }
+        
+    };
+    
+    public static final void sort(List<String> sortingTarget)
+    {
+        
+        Collections.sort(sortingTarget, stringComp);
         
     }
     
-    public static void sortByAsc(List<Integer> sortingTarget)
+    public static final void sortByAsc(List<Integer> sortingTarget)
     {
-        Comparator<Integer> comparator = new Comparator<Integer>() {
-            
-            public int compare(Integer arg0, Integer arg1)
-            {
-                
-                int result = 0;
-                
-                if (arg0 < arg1)
-                {
-                    result = -1;
-                }
-                else if (arg0 > arg1)
-                {
-                    result = 1;
-                }
-                else
-                {
-                    result = 0;
-                }
-                
-                return result;
-            }
-            
-        };
-        
-        Collections.sort(sortingTarget, comparator);
+        Collections.sort(sortingTarget, intAscComp);
     }
     
-    public static void sortByDesc(List<Integer> sortingTarget)
+    public static final void sortByDesc(List<Integer> sortingTarget)
     {
-        Comparator<Integer> comparator = new Comparator<Integer>() {
-            
-            public int compare(Integer arg0, Integer arg1)
-            {
-                
-                int result = 0;
-                
-                if (arg0 < arg1)
-                {
-                    result = 1;
-                }
-                else if (arg0 > arg1)
-                {
-                    result = -1;
-                }
-                else
-                {
-                    result = 0;
-                }
-                
-                return result;
-            }
-            
-        };
         
-        Collections.sort(sortingTarget, comparator);
+        Collections.sort(sortingTarget, intDescComp);
     }
-    
     
     @SuppressWarnings({ "rawtypes", "unchecked" })
-    public static void sort(List<Object> sortingTarget, String sortingField, boolean ascending)
-    { 
+    public static final void sort(List<Object> sortingTarget, String sortingField, boolean ascending)
+    {
         
         Comparator totalComapartor = new ClassComparator(sortingField, ascending);
         
