cb6c549108c322cf4c9a31a3f7d4e7e6247ea41c
AhnSeongHyun <sh84.ahn@gmail.com>
2013-03-05 10:43:42 +0900

    [add] class for config

---
+++ src/main/java/org/espressootr/lib/conf/ConfigMap.java
@@ -0,0 +1,80 @@
+package org.espressootr.lib.conf;
+
+import java.util.Map;
+
+public class ConfigMap
+{
+    private Map<String, Object> confKV = null;
+    
+    @SuppressWarnings("unused")
+    private ConfigMap()
+    {
+        throw new AssertionError();
+        
+    }
+    
+    public ConfigMap(Map<String, Object> confKV)
+    {
+        this.confKV = confKV;
+    }
+    
+    public void set(String key, Object value)
+    {
+        if (this.confKV != null)
+        {
+            this.confKV.put(key, value);
+        }
+    }
+    
+    public boolean exist(String key)
+    {
+        return confKV.containsKey(key);
+    }
+    
+    public void put(String key, Object value)
+    {
+        confKV.put(key, value);
+    }
+    
+    public String getStringValue(String key)
+    {
+        String value = "";
+        
+        if (confKV.containsKey(key))
+        {
+            value = confKV.get(key).toString();
+        }
+        else
+        {
+            value = "";
+        }
+        
+        return value;
+    }
+    
+    public String[] getStringArrayValue(String key)
+    {
+        return (String[]) confKV.get(key);
+    }
+    
+    public int[] getIntArrayValue(String key)
+    {
+        return (int[]) confKV.get(key);
+    }
+    
+    public int getIntValue(String key)
+    {
+        return Integer.valueOf(confKV.get(key).toString());
+    }
+    
+    public void clear()
+    {
+        this.confKV.clear();
+    }
+    
+    @Override
+    public String toString()
+    {
+        return this.confKV.toString();
+    }
+}
