f23762460b8b10a03037937664a85662dbb29358
AhnSeongHyun <sh84.ahn@gmail.com>
2013-03-05 17:19:29 +0900

    [modify] add index param in unicode class

--- src/main/java/org/espressootr/lib/encoding/Unicode.java
+++ src/main/java/org/espressootr/lib/encoding/Unicode.java
@@ -3,9 +3,9 @@ package org.espressootr.lib.encoding;
 
 public final class Unicode
 {
-    public static String getUnicodeFromString(String source)
+    public static String getUnicodeFromString(String source, int index)
     {
-	return String.format("0X" + "%04X", source.codePointAt(0));
+	return String.format("0X" + "%04X", source.codePointAt(index));
     }
  
 }
